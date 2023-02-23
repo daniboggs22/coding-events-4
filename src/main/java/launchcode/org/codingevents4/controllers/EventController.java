ffb484dda9133bfd1fd1d318785adff9da77aef3
Daneille Boggs <daniboggs22@gmail.com>
2023-02-23 11:30:05 -0600

    created delete.html and updated Event Controller. Checkbox is not functioning correctly.

--- src/main/java/launchcode/org/codingevents4/controllers/EventController.java
+++ src/main/java/launchcode/org/codingevents4/controllers/EventController.java
@@ -32,5 +32,21 @@ public class EventController {
         EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
 }
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        //need to pass in a collection of events to delete any
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+}
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds ){
+    if(eventIds != null) {
+        for (int id : eventIds) {
+            EventData.remove(id);
+        }
+    }
+        return "redirect:";
+    }
 
 }
